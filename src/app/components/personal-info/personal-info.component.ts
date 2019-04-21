dd0e109be06264e351d35b14b57810c4d3e4202c
Robert Nino <robert@l-nino.net>
2019-04-21 07:48:30 -0500

    formatted

--- src/app/components/personal-info/personal-info.component.ts
+++ src/app/components/personal-info/personal-info.component.ts
@@ -6,84 +6,85 @@ import { RegexSettings } from '../../classes/regexes';
 
 @Component({
   selector: 'app-personal-info',
-  providers:[DatePipe],
+  providers: [DatePipe],
   templateUrl: './personal-info.component.html',
   styleUrls: ['./personal-info.component.scss']
 })
 export class PersonalInfoComponent implements OnInit {
-	public header;
+  public header;
   private newPlayerHeader = "Enter Player Information";
-	private returningPlayerHeader = "Verify Player Information";
-	public player;
+  private returningPlayerHeader = "Verify Player Information";
+  public player;
   public view;
-	public isEdit;
-	public emailPatterm = RegexSettings.email.pattern;
-	public DOB = { days:[], months: MonthList, years:[] };
+  public isEdit;
+  public emailPatterm = RegexSettings.email.pattern;
+  // public email = RegexSettings.allowedCharacters.email;
+  public DOB = { days: [], months: MonthList, years: [] };
 
   constructor(private ps: PlayerService, private datePipe: DatePipe) {
-  	this.ps.player.subscribe(data => {
-  		if(data) this.player = data;
+    this.ps.player.subscribe(data => {
+      if (data) this.player = data;
       // if(this.player.dob.year) 
-  		this.header = (this.player && this.player.status == 'RETURN')?this.returningPlayerHeader:(this.player && this.player.status == 'NEW')?this.newPlayerHeader:'';
-  	});
-  	this.ps.view.subscribe( data => {
-      if(data) this.view = data;
+      this.header = (this.player && this.player.status == 'RETURN') ? this.returningPlayerHeader : (this.player && this.player.status == 'NEW') ? this.newPlayerHeader : '';
+    });
+    this.ps.view.subscribe(data => {
+      if (data) this.view = data;
       // if(this.view) console.log(this.view);
     });
-    this.ps.etiting.subscribe(data=>{ this.isEdit = data; });
+    this.ps.etiting.subscribe(data => { this.isEdit = data; });
     let days = [];
     let months = MonthList;
     let years = [];
-  	for (let i: number = 1; i < 32 + 1; i++)
-    	days.push(i);
+    for (let i: number = 1; i < 32 + 1; i++)
+      days.push(i);
     this.DOB.days = days;
     // this.player.dob.day = (this.player.dob.day)?this.player.dob.day:this.DOB.days[0];
-    this.player.dob.day = (this.player.dob.day)?this.player.dob.day:0;
+    this.player.dob.day = (this.player.dob.day) ? this.player.dob.day : 0;
     // this.player.dob.day = 0;
 
     this.DOB.months = months;
     // this.player.dob.month = (this.player.dob.month)?this.player.dob.month:this.DOB.months[0].value;
     // this.player.dob.month = 0;
-    this.player.dob.month = (this.player.dob.month)? this.player.dob.month : 0;
+    this.player.dob.month = (this.player.dob.month) ? this.player.dob.month : 0;
 
     let yr = (new Date()).getFullYear() - 29;
     for (let y: number = yr; y < (new Date()).getFullYear(); y++)
-    	years.push(y);
+      years.push(y);
 
     this.DOB.years = years;
     // this.player.dob.year = (this.player.dob.year)?this.player.dob.year:this.DOB.years[0];
-    this.player.dob.year = (this.player.dob.year)? this.player.dob.year : 0;
-   }
+    this.player.dob.year = (this.player.dob.year) ? this.player.dob.year : 0;
+  }
 
   ngOnInit() {
-  	// console.log(this.DOB);
+    // console.log(this.DOB);
   }
-  submit(){
+  submit() {
 
-		this.ps.changePlayer(this.player);
-		if(this.isEdit) 
+    this.ps.changePlayer(this.player);
+    if (this.isEdit)
       return this.ps.changeView('REVIEW');
     this.ps.changeView('ADDY');
   }
-  onMonthChange(month){
-  	console.log(month);
-  	let numDays = MonthList[month - 1].days;
-  	let days = [];
-  	for (let i: number = 1; i < numDays + 1; i++)
-    	days.push(i);
+  onMonthChange(month) {
+    console.log(month);
+    let numDays = MonthList[month - 1].days;
+    let days = [];
+    for (let i: number = 1; i < numDays + 1; i++)
+      days.push(i);
     this.DOB.days = days;
   }
-	// TODO: account for leap years if propper year is selected
-  isLeapYear(year){
- 		return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+  // TODO: account for leap years if propper year is selected
+  isLeapYear(year) {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
   }
 
-  onDobChange(){
-  	let day = this.player.dob.day;
-  	let month = this.player.dob.month;
-  	let year = this.player.dob.year;
-  	if(!year || !month ||!day) return;
-  	this.player.dob.string = this.datePipe.transform(new Date(String(day).concat('/').concat(String(month)).concat('/').concat(String(year))),'dd/MM/yyyy');
+  onDobChange() {
+    let day = this.player.dob.day;
+    let month = this.player.dob.month;
+    let year = this.player.dob.year;
+    if (!year || !month || !day) return;
+    this.player.dob.string = this.datePipe.transform(new Date(String(day).concat('/').concat(String(month)).concat('/').concat(String(year))), 'dd/MM/yyyy');
   }
 
 }
--- src/app/components/select-school/select-school.component.ts
+++ src/app/components/select-school/select-school.component.ts
@@ -9,37 +9,39 @@ import { PlayerService } from '../../services/player/player.service';
 export class SelectSchoolComponent implements OnInit {
   public schools = {
     DSHS: {
-        name: 'High School',
-        value: 'DSHS'
+      name: 'High School',
+      value: 'DSHS'
     },
     DSMS: {
-        name: "Middle School",
-        value: "DSMS"
+      name: "Middle School",
+      value: "DSMS"
     }
   };
   public player;
   public view;
   public header = "What school does the player attent?";
-  constructor(private ps: PlayerService) {
+  constructor(
+    private ps: PlayerService
+  ) {
     this.ps.player.subscribe(data => {
-      if(data) this.player = data;
+      if (data) this.player = data;
     });
-    this.ps.view.subscribe( data => {
-      if(data) this.view = data;
+    this.ps.view.subscribe(data => {
+      if (data) this.view = data;
       // if(this.view) console.log(this.view);
     });
   }
 
-  ngOnInit() {   }
+  ngOnInit() { }
 
-  assignSchool(val){
-      if(!val) return;
-      // console.log(val);
-      if(this.player) {
-        this.player.school = val;
-        this.ps.changePlayer(this.player);
-        this.ps.changeView('NAT_ID');
-      }
+  assignSchool(val) {
+    if (!val) return;
+    // console.log(val);
+    if (this.player) {
+      this.player.school = val;
+      this.ps.changePlayer(this.player);
+      this.ps.changeView('NAT_ID');
+    }
   }
 
 }
