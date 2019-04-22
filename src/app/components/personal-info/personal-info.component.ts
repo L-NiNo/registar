b728b94ccb58ca75fe09a72b7648f104973cb655
Robert Nino <robert@l-nino.net>
2019-04-22 00:25:43 -0500

    use class and added leap year check

--- src/app/components/personal-info/personal-info.component.ts
+++ src/app/components/personal-info/personal-info.component.ts
@@ -21,15 +21,16 @@ export class PersonalInfoComponent implements OnInit {
   // public email = RegexSettings.allowedCharacters.email;
   public DOB = { days: [], months: MonthList, years: [] };
 
-  constructor(private ps: PlayerService, private datePipe: DatePipe) {
+  constructor(
+    private ps: PlayerService,
+    private datePipe: DatePipe
+  ) {
     this.ps.player.subscribe(data => {
       if (data) this.player = data;
-      // if(this.player.dob.year) 
       this.header = (this.player && this.player.status == 'RETURN') ? this.returningPlayerHeader : (this.player && this.player.status == 'NEW') ? this.newPlayerHeader : '';
     });
     this.ps.view.subscribe(data => {
       if (data) this.view = data;
-      // if(this.view) console.log(this.view);
     });
     this.ps.etiting.subscribe(data => { this.isEdit = data; });
     let days = [];
@@ -38,13 +39,9 @@ export class PersonalInfoComponent implements OnInit {
     for (let i: number = 1; i < 32 + 1; i++)
       days.push(i);
     this.DOB.days = days;
-    // this.player.dob.day = (this.player.dob.day)?this.player.dob.day:this.DOB.days[0];
     this.player.dob.day = (this.player.dob.day) ? this.player.dob.day : 0;
-    // this.player.dob.day = 0;
 
     this.DOB.months = months;
-    // this.player.dob.month = (this.player.dob.month)?this.player.dob.month:this.DOB.months[0].value;
-    // this.player.dob.month = 0;
     this.player.dob.month = (this.player.dob.month) ? this.player.dob.month : 0;
 
     let yr = (new Date()).getFullYear() - 29;
@@ -52,39 +49,51 @@ export class PersonalInfoComponent implements OnInit {
       years.push(y);
 
     this.DOB.years = years;
-    // this.player.dob.year = (this.player.dob.year)?this.player.dob.year:this.DOB.years[0];
     this.player.dob.year = (this.player.dob.year) ? this.player.dob.year : 0;
   }
 
   ngOnInit() {
-    // console.log(this.DOB);
   }
-  submit() {
 
+  submit() {
     this.ps.changePlayer(this.player);
     if (this.isEdit)
       return this.ps.changeView('REVIEW');
     this.ps.changeView('ADDY');
   }
+
   onMonthChange(month) {
     console.log(month);
+    if (month === '0')
+      this.player.dob.month = 0;
+    if (!month || month === '0') return;
     let numDays = MonthList[month - 1].days;
     let days = [];
     for (let i: number = 1; i < numDays + 1; i++)
       days.push(i);
     this.DOB.days = days;
   }
-  // TODO: account for leap years if propper year is selected
+
+  checkYear(year) {
+    if (this.player.dob.month === 2) {
+      if (this.isLeapYear(year)) {
+        this.DOB.days.push(29);
+      } else {
+        this.onMonthChange(this.player.dob.month);
+      }
+    }
+  }
+
   isLeapYear(year) {
     return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
   }
 
   onDobChange() {
-    let day = this.player.dob.day;
-    let month = this.player.dob.month;
-    let year = this.player.dob.year;
-    if (!year || !month || !day) return;
-    this.player.dob.string = this.datePipe.transform(new Date(String(day).concat('/').concat(String(month)).concat('/').concat(String(year))), 'dd/MM/yyyy');
+    let day = Number(this.player.dob.day);
+    let month = Number(this.player.dob.month);
+    let year = Number(this.player.dob.year);
+    if (!year || !month || !day) return this.player.dob.clearBirthday();
+    this.player.dob.setBirthday();
   }
 
 }
