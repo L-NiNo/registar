e60d30419a21f5dd5306ac03cfb3242f55f741ff
Robert Nino <robert@l-nino.net>
2019-04-22 00:22:42 -0500

    use player class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlayerService } from './services/player/player.service';
 import { setTheme } from 'ngx-bootstrap/utils';
+import { Player } from './classes/Player';
 
 @Component({
   selector: 'app-root',
@@ -25,106 +26,99 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(){
-	  let player = {
-	  	status: undefined,
-	  	nationalId:'',
-	  	id:'',
-	  	fname:'',
-	  	lname:'',
-	  	email: '',
-	  	address: {},
-	  	dob:{ day: null, month: null, year: null},
-	  	school:null,
-	  	grade: null,
-	  	year:'',
-	  	emergency: { primary: {}, secondary: {}},
-	  	consents:{
-			  REP:false,
-			LIABILITY:false,
-			CONDUCT_LOCAL:false,
-			CONDUCT_LEAGUE:false,
-			FEES:false,
-			IMAGE:false
-		},
-	  	medical:{},
-	  	guardian:{ primary: {}, secondary: {}}
-	  };
-	  let player2 = {
-	  	status: undefined,
-	  	nationalId:'',
-	  	id:'',
-	  	fname:'Jon',
-	  	lname:'Doe',
-	  	email: 'xxx@xx.com',
-	  	address: {
-	  		street1:'1230 St ln',
-	  		street2:'apt 130',
-	  		city:'Austin',
-	  		state:'Texas',
-	  		zip:'78737'
-	  	},
-	  	dob:{ 
-	  		day: 9, 
-	  		month: 12, 
-	  		year: 2002,
-	  		string:'12/09/2002'
-	  	},
-	  	school:'DSHS',
-	  	grade: 9,
-	  	year: this.getSeasonYear(),
-	  	emergency: { 
-	  		primary: {
-	  			name:'james man',
-	  			relationship:'uncle',
-	  			phone:'5125736325'
-	  	}, 
-	  		secondary: {
-	  			name:'jane man',
-	  			relationship:'aunt',
-	  			phone:'5121369856'
-	  		}
-	  	},
-	  	consents:{
-			REP:true,
-	  		LIABILITY:true,
-	  		CONDUCT_LOCAL:true,
-	  		CONDUCT_LEAGUE:true,
-	  		FEES:true,
-	  		IMAGE:true
-	  	},
-	  	medical:{ 
-	  		physician:'Sean Ditty',
-	  		physicianNum:'3652659685',
-	  		insurance:'cigna',
-	  		policy:'0A15795658',
-	  		callNum:'18003336666',
-	  		allergies:'N/A',
-	  		conditions:'N/A',
-	  		medications:'N/A'
-	  	},
-	  	guardian:{ 
-	  		primary: {
-	  			name:'Mommy Doe',
-	  			email:'zzz@xx.com',
-	  			phone:'3596589963'
-	  		}, 
-	  		secondary: {
-	  			name:'Daddy Doe',
-	  			phone:'9876541259'
-	  		}
-	  	}
-	  };
+	//   let player = {
+	//   	status: undefined,
+	//   	nationalId:'',
+	//   	id:'',
+	//   	fname:'',
+	//   	lname:'',
+	//   	email: '',
+	//   	address: {},
+	//   	dob:{ day: null, month: null, year: null},
+	//   	school:null,
+	//   	grade: null,
+	//   	year:'',
+	//   	emergency: { primary: {}, secondary: {}},
+	//   	consents:{
+	// 		  REP:false,
+	// 		LIABILITY:false,
+	// 		CONDUCT_LOCAL:false,
+	// 		CONDUCT_LEAGUE:false,
+	// 		FEES:false,
+	// 		IMAGE:false
+	// 	},
+	//   	medical:{},
+	//   	guardian:{ primary: {}, secondary: {}}
+	//   };
+	  let player = new Player();
+	//   let player2 = {
+	//   	status: undefined,
+	//   	nationalId:'',
+	//   	id:'',
+	//   	fname:'Jon',
+	//   	lname:'Doe',
+	//   	email: 'xxx@xx.com',
+	//   	address: {
+	//   		street1:'1230 St ln',
+	//   		street2:'apt 130',
+	//   		city:'Austin',
+	//   		state:'Texas',
+	//   		zip:'78737'
+	//   	},
+	//   	dob:{ 
+	//   		day: 9, 
+	//   		month: 12, 
+	//   		year: 2002,
+	//   		string:'12/09/2002'
+	//   	},
+	//   	school:'DSHS',
+	//   	grade: 9,
+	//   	year: this.getSeasonYear(),
+	//   	emergency: { 
+	//   		primary: {
+	//   			name:'james man',
+	//   			relationship:'uncle',
+	//   			phone:'5125736325'
+	//   	}, 
+	//   		secondary: {
+	//   			name:'jane man',
+	//   			relationship:'aunt',
+	//   			phone:'5121369856'
+	//   		}
+	//   	},
+	//   	consents:{
+	// 		REP:true,
+	//   		LIABILITY:true,
+	//   		CONDUCT_LOCAL:true,
+	//   		CONDUCT_LEAGUE:true,
+	//   		FEES:true,
+	//   		IMAGE:true
+	//   	},
+	//   	medical:{ 
+	//   		physician:'Sean Ditty',
+	//   		physicianNum:'3652659685',
+	//   		insurance:'cigna',
+	//   		policy:'0A15795658',
+	//   		callNum:'18003336666',
+	//   		allergies:'N/A',
+	//   		conditions:'N/A',
+	//   		medications:'N/A'
+	//   	},
+	//   	guardian:{ 
+	//   		primary: {
+	//   			name:'Mommy Doe',
+	//   			email:'zzz@xx.com',
+	//   			phone:'3596589963'
+	//   		}, 
+	//   		secondary: {
+	//   			name:'Daddy Doe',
+	//   			phone:'9876541259'
+	//   		}
+	//   	}
+	//   };
 
 	  this.ps.changePlayer(player);
 	  // console.log(this);
-  }
-
-  private getSeasonYear(){
-  	const today = new Date();
-  	if(today.getMonth()+1 >= 7)
-	  	return today.getFullYear().toString() + '-' + (today.getFullYear()+1).toString();
-	  else 
-	  	return (today.getFullYear()-1).toString() + '-' + today.getFullYear().toString();
   }
 
   isRegOpen(){
