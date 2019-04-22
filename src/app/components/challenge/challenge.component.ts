10e09ca399b7fd17bf342384296fb29a2e640120
Robert Nino <robert@l-nino.net>
2019-04-22 00:23:41 -0500

    incorrect found flow

--- src/app/components/challenge/challenge.component.ts
+++ src/app/components/challenge/challenge.component.ts
@@ -3,9 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { PlayerService } from '../../services/player/player.service';
 
 @Component({
-  selector: 'app-challenge',
-  templateUrl: './challenge.component.html',
-  styleUrls: ['./challenge.component.scss']
+	selector: 'app-challenge',
+	templateUrl: './challenge.component.html',
+	styleUrls: ['./challenge.component.scss']
 })
 export class ChallengeComponent implements OnInit {
 	public name;
@@ -14,36 +14,49 @@ export class ChallengeComponent implements OnInit {
 	public player;
 	public view;
 	public header;
-  constructor(private http: HttpClient, private ps: PlayerService) {
-  	this.ps.player.subscribe(data => {
-  		if(data) this.player = data;
-  		this.header = 'Verify Player Information';
-  	});
-  	this.ps.view.subscribe( data => {
-      if(data) this.view = data;
-      // if(this.view) console.log(this.view);
-    });
-   }
+	public data;
+	constructor(
+		private http: HttpClient, 
+		private ps: PlayerService
+	) {
+		this.ps.player.subscribe(data => {
+			if (data) this.player = data;
+			this.header = 'Verify Player Information';
+		});
+		this.ps.view.subscribe(data => {
+			if (data) this.view = data;
+			// if(this.view) console.log(this.view);
+		});
+		this.ps.challenge.subscribe(data => {
+			if (data) this.data = data;
+		});
+	}
 
 	ngOnInit() {
 	}
 
-	submit(){
+	submit() {
 		this.loading = true;
 		let data = { player: this.player.nationalId, lname: this.name, email: this.email };
-    	this.http.post('/php/challenge.php', data).subscribe( 
-    		(data:any) => {
-      			console.log("success", data);
-      			this.loading = false;
-      			this.ps.changePlayer(data.player);
-      			this.ps.changeView('REVIEW');
-    		},
-    		error => {
-      			console.log("error");
-      			this.loading = false;
-      		}
-      	);
+		this.http.post('/php/challenge.php', data).subscribe(
+			(data: any) => {
+				console.log("success", data);
+				this.loading = false;
+				this.ps.changePlayer(data.player);
+				this.ps.changeView('REVIEW');
+			},
+			error => {
+				console.log("error");
+				this.loading = false;
+			}
+		);
 	}
-  	
+
+	goBack(){
+		this.ps.changePlayer(null);
+		this.ps.changeChallenge(null);
+		this.ps.changeView('INIT');
+	}
+
 
 }
