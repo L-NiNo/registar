a7c7abaa6d28a3f71953a57fdaeaeede4064aa2d
Robert Nino <robert@l-nino.net>
2019-05-19 15:16:14 -0500

    challengecomp updated

--- src/app/components/challenge/challenge.component.ts
+++ src/app/components/challenge/challenge.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PlayerService } from '../../services/player/player.service';
+import { Birthdate } from 'src/app/classes/Birthdate';
+import { RegexSettings } from '../../classes/regexes';
+import { Player } from 'src/app/classes/Player';
 
 @Component({
 	selector: 'app-challenge',
@@ -8,17 +11,23 @@ import { PlayerService } from '../../services/player/player.service';
 	styleUrls: ['./challenge.component.scss']
 })
 export class ChallengeComponent implements OnInit {
-	public name;
+	public dob: Birthdate;
 	public email;
 	public loading = false;
 	public player;
 	public view;
 	public header;
 	public data;
+	public error;
+	public emailPatterm = RegexSettings.email.pattern.source;
 	constructor(
-		private http: HttpClient, 
+		private http: HttpClient,
 		private ps: PlayerService
 	) {
+		// this.dob = '01/20/2002';
+		this.dob = new Birthdate(20, 1, 2002);
+		this.email = 'xxx@xx.com';
+
 		this.ps.player.subscribe(data => {
 			if (data) this.player = data;
 			this.header = 'Verify Player Information';
@@ -37,26 +46,29 @@ export class ChallengeComponent implements OnInit {
 
 	submit() {
 		this.loading = true;
-		let data = { player: this.player.nationalId, lname: this.name, email: this.email };
-		this.http.post('/php/challenge.php', data).subscribe(
-			(data: any) => {
-				console.log("success", data);
+
+		let data = { player: this.player.nationalId, dob: this.dob.birthday.replace(/\//g, '.'), email: this.email.toUpperCase() };
+		this.http.post('/api/verify.php', data).subscribe(
+			(response: any) => {
+				console.log("success", response);
 				this.loading = false;
-				this.ps.changePlayer(data.player);
+				this.ps.changePlayer(Object.assign({}, this.player, response.data));
 				this.ps.changeView('REVIEW');
 			},
 			error => {
 				console.log("error");
+				this.error = true;
 				this.loading = false;
 			}
 		);
 	}
 
-	goBack(){
-		this.ps.changePlayer(null);
+	goBack() {
+		this.ps.changePlayer(new Player());
 		this.ps.changeChallenge(null);
 		this.ps.changeView('INIT');
 	}
+
 
 
 }
