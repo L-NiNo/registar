96da182b8c4739881c40e1c4ebad45699c87b2f0
Robert Nino <robert@l-nino.net>
2019-05-19 15:52:54 -0500

    update address comp

--- src/app/components/address/address.component.ts
+++ src/app/components/address/address.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PlayerService } from '../../services/player/player.service';
-
-const stateArray = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
+import { StatesObj } from 'src/app/models/States';
 
 @Component({
   selector: 'app-address',
@@ -15,32 +14,41 @@ export class AddressComponent implements OnInit {
   public player;
   public isEdit;
   public view;
-  public states = stateArray;
+  public states;
 
   constructor(private ps: PlayerService) {
     this.ps.player.subscribe(data => {
-      if(data) this.player = data;
-      this.player.address.state = (this.player && this.player.address && this.player.address.state)?this.player.address.state:'Texas';
-      this.header = (this.player && this.player.status == 'RETURN')?this.returningPlayerHeader:(this.player && this.player.status == 'NEW')?this.newPlayerHeader:'';
+      if (data) this.player = data;
+      this.player.address.state = (this.player && this.player.address && this.player.address.state) ? this.player.address.state : 'Texas';
+      this.header = (this.player && this.player.status == 'RETURN') ? this.returningPlayerHeader : (this.player && this.player.status == 'NEW') ? this.newPlayerHeader : '';
     });
-    this.ps.view.subscribe( data => {
-      if(data) this.view = data;
+    this.ps.view.subscribe(data => {
+      if (data) this.view = data;
     });
-    this.ps.etiting.subscribe(data=>{ this.isEdit = data; });
+    this.ps.etiting.subscribe(data => { this.isEdit = data; });
+    this.states = this.buildStatesArray()
   }
 
   ngOnInit() {
   }
-  
-  submit(){
+
+  submit() {
     this.ps.changePlayer(this.player);
-    if(this.isEdit) 
+    if (this.isEdit)
       return this.ps.changeView('REVIEW');
     this.ps.changeView('GUARD');
   }
 
-  back(){
+  back() {
     this.ps.changeView('PI');
+  }
+
+  buildStatesArray() {
+    let _states = [];
+    Object.keys(StatesObj).forEach((val) => {
+      _states.push({ id: val, name: StatesObj[val] });
+    });
+    return _states;
   }
 
 }
