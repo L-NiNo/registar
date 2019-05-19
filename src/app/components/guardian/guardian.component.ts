9a3233d4c714275064c83bc03e1ef32e53d837f9
Robert Nino <robert@l-nino.net>
2019-05-19 16:17:59 -0500

    update guardian comp

--- src/app/components/guardian/guardian.component.ts
+++ src/app/components/guardian/guardian.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PlayerService } from '../../services/player/player.service';
+import { RegexSettings } from 'src/app/classes/regexes';
 
 @Component({
   selector: 'app-guardian',
@@ -13,29 +14,40 @@ export class GuardianComponent implements OnInit {
   public player;
   public view;
   public isEdit;
+  public secondary;
+  public emailPattern = RegexSettings.email.pattern.source;
 
   constructor(private ps: PlayerService) {
     this.ps.player.subscribe(data => {
-      if(data) this.player = data;
-      this.header = (this.player && this.player.status == 'RETURN')?this.returningPlayerHeader:(this.player && this.player.status == 'NEW')?this.newPlayerHeader:'';
+      if (data) this.player = data;
+      this.ngOnInit();
+      this.header = (this.player && this.player.status == 'RETURN') ? this.returningPlayerHeader : (this.player && this.player.status == 'NEW') ? this.newPlayerHeader : '';
     });
-    this.ps.view.subscribe( data => {
-      if(data) this.view = data;
+    this.ps.view.subscribe(data => {
+      if (data) this.view = data;
     });
-    this.ps.etiting.subscribe(data=>{ this.isEdit = data; });
+    this.ps.etiting.subscribe(data => { this.isEdit = data; });
   }
 
   ngOnInit() {
+    if (this.player && this.player.guardian && this.player.guardian.secondary && this.player.guardian.secondary) {
+      if ((this.player.guardian.secondary.phone && this.player.guardian.secondary.phone !== "") ||
+        (this.player.guardian.secondary.name && this.player.guardian.secondary.name !== "") ||
+        (this.player.guardian.secondary.eamil && this.player.guardian.secondary.email !== "")) {
+        this.secondary = true;
+      }
+
+    }
   }
 
-  submit(){
+  submit() {
     this.ps.changePlayer(this.player);
-    if(this.isEdit) 
+    if (this.isEdit)
       return this.ps.changeView('REVIEW');
     this.ps.changeView('EMERGENCY');
   }
 
-  back(){
+  back() {
     this.ps.changeView('ADDY');
   }
 
