8553167f438c430189b06bd58c959cfe8eb6d926
Robert Nino <robert@l-nino.net>
2019-06-18 07:16:52 -0500

    tranfer pc checkin

--- src/app/services/player/player.service.ts
+++ src/app/services/player/player.service.ts
@@ -1,33 +1,40 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Player } from 'src/app/classes/Player';
 
 
 @Injectable()
 export class PlayerService {
 
 	private _player = new BehaviorSubject<any>(null);
-  // private _view = new BehaviorSubject<any>('CONSENT');
-  private _view = new BehaviorSubject<any>('INIT');
+	private _view = new BehaviorSubject<any>('INIT');
+	// private _view = new BehaviorSubject<any>('PI');
 	private _editing = new BehaviorSubject<boolean>(false);
-	
+	private _challenge = new BehaviorSubject<any>(null);
+
 	public player = this._player.asObservable();
-  public view = this._view.asObservable();
+	public view = this._view.asObservable();
 	public etiting = this._view.asObservable();
+	public challenge = this._challenge.asObservable();
 
 
-  	constructor() { }
+	constructor() { }
 
-  	changePlayer(data){
-  		this._player.next(data);
-  	}
+	changePlayer(data: Player) {
+		this._player.next(data);
+	}
 
-  	changeView(data){
-      this._view.next(data);
-    }
+	changeView(data) {
+		this._view.next(data);
+	}
 
-    changeEditing(data){
-  		this._view.next(data);
-  	}
+	changeEditing(data) {
+		this._editing.next(data);
+	}
+
+	changeChallenge(data) {
+		this._challenge.next(data);
+	}
 
 
 }
