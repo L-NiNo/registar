5728f9118589b12dfb7bee0d31905d38105a175b
Robert Nino <robert@l-nino.net>
2019-10-03 22:24:14 -0500

    open fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,131 +4,129 @@ import { setTheme } from 'ngx-bootstrap/utils';
 import { Player } from './classes/Player';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  public player;
-  public view;
-  
+	public player;
+	public view;
 
-  constructor(private ps: PlayerService) { 
-  	 setTheme('bs4');
-  	this.ps.player.subscribe( data => {
-  		if(data) this.player = data;
-  		// if(this.player) console.log(this.player);
-  	});
-  	this.ps.view.subscribe( data => {
-  		if(data) this.view = data;
-  		// if(this.view) console.log(this.view);
-  	});
-  }
 
-  ngOnInit(){
-	//   let player = {
-	//   	status: undefined,
-	//   	nationalId:'',
-	//   	id:'',
-	//   	fname:'',
-	//   	lname:'',
-	//   	email: '',
-	//   	address: {},
-	//   	dob:{ day: null, month: null, year: null},
-	//   	school:null,
-	//   	grade: null,
-	//   	year:'',
-	//   	emergency: { primary: {}, secondary: {}},
-	//   	consents:{
-	// 		  REP:false,
-	// 		LIABILITY:false,
-	// 		CONDUCT_LOCAL:false,
-	// 		CONDUCT_LEAGUE:false,
-	// 		FEES:false,
-	// 		IMAGE:false
-	// 	},
-	//   	medical:{},
-	//   	guardian:{ primary: {}, secondary: {}}
-	//   };
-	  let player = new Player();
-	//   let player2 = {
-	//   	status: undefined,
-	//   	nationalId:'',
-	//   	id:'',
-	//   	fname:'Jon',
-	//   	lname:'Doe',
-	//   	email: 'xxx@xx.com',
-	//   	address: {
-	//   		street1:'1230 St ln',
-	//   		street2:'apt 130',
-	//   		city:'Austin',
-	//   		state:'Texas',
-	//   		zip:'78737'
-	//   	},
-	//   	dob:{ 
-	//   		day: 9, 
-	//   		month: 12, 
-	//   		year: 2002,
-	//   		string:'12/09/2002'
-	//   	},
-	//   	school:'DSHS',
-	//   	grade: 9,
-	//   	year: this.getSeasonYear(),
-	//   	emergency: { 
-	//   		primary: {
-	//   			name:'james man',
-	//   			relationship:'uncle',
-	//   			phone:'5125736325'
-	//   	}, 
-	//   		secondary: {
-	//   			name:'jane man',
-	//   			relationship:'aunt',
-	//   			phone:'5121369856'
-	//   		}
-	//   	},
-	//   	consents:{
-	// 		REP:true,
-	//   		LIABILITY:true,
-	//   		CONDUCT_LOCAL:true,
-	//   		CONDUCT_LEAGUE:true,
-	//   		FEES:true,
-	//   		IMAGE:true
-	//   	},
-	//   	medical:{ 
-	//   		physician:'Sean Ditty',
-	//   		physicianNum:'3652659685',
-	//   		insurance:'cigna',
-	//   		policy:'0A15795658',
-	//   		callNum:'18003336666',
-	//   		allergies:'N/A',
-	//   		conditions:'N/A',
-	//   		medications:'N/A'
-	//   	},
-	//   	guardian:{ 
-	//   		primary: {
-	//   			name:'Mommy Doe',
-	//   			email:'zzz@xx.com',
-	//   			phone:'3596589963'
-	//   		}, 
-	//   		secondary: {
-	//   			name:'Daddy Doe',
-	//   			phone:'9876541259'
-	//   		}
-	//   	}
-	//   };
+	constructor(private ps: PlayerService) {
+		setTheme('bs4');
+		this.ps.player.subscribe(data => {
+			if (data) this.player = data;
+			// if(this.player) console.log(this.player);
+		});
+		this.ps.view.subscribe(data => {
+			if (data) this.view = data;
+			// if(this.view) console.log(this.view);
+		});
+	}
 
-	  this.ps.changePlayer(player);
-	  // console.log(this);
-  }
+	ngOnInit() {
+		//   let player = {
+		//   	status: undefined,
+		//   	nationalId:'',
+		//   	id:'',
+		//   	fname:'',
+		//   	lname:'',
+		//   	email: '',
+		//   	address: {},
+		//   	dob:{ day: null, month: null, year: null},
+		//   	school:null,
+		//   	grade: null,
+		//   	year:'',
+		//   	emergency: { primary: {}, secondary: {}},
+		//   	consents:{
+		// 		  REP:false,
+		// 		LIABILITY:false,
+		// 		CONDUCT_LOCAL:false,
+		// 		CONDUCT_LEAGUE:false,
+		// 		FEES:false,
+		// 		IMAGE:false
+		// 	},
+		//   	medical:{},
+		//   	guardian:{ primary: {}, secondary: {}}
+		//   };
+		let player = new Player();
+		//   let player2 = {
+		//   	status: undefined,
+		//   	nationalId:'',
+		//   	id:'',
+		//   	fname:'Jon',
+		//   	lname:'Doe',
+		//   	email: 'xxx@xx.com',
+		//   	address: {
+		//   		street1:'1230 St ln',
+		//   		street2:'apt 130',
+		//   		city:'Austin',
+		//   		state:'Texas',
+		//   		zip:'78737'
+		//   	},
+		//   	dob:{ 
+		//   		day: 9, 
+		//   		month: 12, 
+		//   		year: 2002,
+		//   		string:'12/09/2002'
+		//   	},
+		//   	school:'DSHS',
+		//   	grade: 9,
+		//   	year: this.getSeasonYear(),
+		//   	emergency: { 
+		//   		primary: {
+		//   			name:'james man',
+		//   			relationship:'uncle',
+		//   			phone:'5125736325'
+		//   	}, 
+		//   		secondary: {
+		//   			name:'jane man',
+		//   			relationship:'aunt',
+		//   			phone:'5121369856'
+		//   		}
+		//   	},
+		//   	consents:{
+		// 		REP:true,
+		//   		LIABILITY:true,
+		//   		CONDUCT_LOCAL:true,
+		//   		CONDUCT_LEAGUE:true,
+		//   		FEES:true,
+		//   		IMAGE:true
+		//   	},
+		//   	medical:{ 
+		//   		physician:'Sean Ditty',
+		//   		physicianNum:'3652659685',
+		//   		insurance:'cigna',
+		//   		policy:'0A15795658',
+		//   		callNum:'18003336666',
+		//   		allergies:'N/A',
+		//   		conditions:'N/A',
+		//   		medications:'N/A'
+		//   	},
+		//   	guardian:{ 
+		//   		primary: {
+		//   			name:'Mommy Doe',
+		//   			email:'zzz@xx.com',
+		//   			phone:'3596589963'
+		//   		}, 
+		//   		secondary: {
+		//   			name:'Daddy Doe',
+		//   			phone:'9876541259'
+		//   		}
+		//   	}
+		//   };
 
-  isRegOpen(){
-  	/* Registration con be done from July until January */
-  	const today = new Date();
-  	const thisMonth = today.getMonth() + 1;
-  	if( thisMonth >= 7)
-  		return true;
-  	return false;
-  }
+		this.ps.changePlayer(player);
+		// console.log(this);
+	}
+
+	isRegOpen() {
+		/* Registration can be done from June until January */
+		const today = new Date();
+		const thisMonth = today.getMonth() + 1;
+		return !(thisMonth >= 6);
+	}
 
 
 }
